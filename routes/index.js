1a38ecccab39592f338b2ba09efbd177c2f0f330
Brennan Glynn <BrennanGlynn@u.boisestate.edu>
2017-03-20 22:06:48 -0600

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'API Basejump: Timestamp microservice' });
+});
+
+router.get('/:date', function (req, res) {
+  var date = req.params.date;
+  var unix = null;
+  var natural = null;
+
+  if (Number(date)) {
+    date = Number(date) * 1000;
+  }
+  date = new Date(date);
+
+  if (date instanceof Date && isFinite(date)) {
+    unix = date.valueOf() / 1000;
+    natural = date.toDateString();
+  }
+
+  res.json({
+    unix: unix,
+    natural: natural
+  });
+});
+
+module.exports = router;
